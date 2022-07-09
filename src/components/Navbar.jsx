184371867e6e5ceb89fa9bb109e64cde24cbd831
wari <nicolaycamacho@gmail.com>
2022-07-09 15:28:11 +0200

    Logo changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
       <div>
-        <img src={Logo} alt='Logo Image' style={{ width: '50px' }} />
+        <img src={Logo} alt='Logo Image' style={{ width: '100px' }} />
       </div>
 
       {/* menu */}
