297f6ebc1bbb89ed7d156c27d2258e85e9b9ec1c
yashwanth2546 <yashwant.k.me@gmail.com>
2022-07-19 11:52:05 +0530

    initial

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
           </div>
           <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
             <div className='sm:text-right text-4xl font-bold'>
-              <p>Hi. I'm Clint, nice to meet you. Please take a look around.</p>
+              <p>Hi. I'm Sai Kumar, nice to meet you. Please take a look around.</p>
             </div>
             <div>
               <p>I am passionate about building excellent software that improves
