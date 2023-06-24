8ba086c500ba0069afad1377237b27d43b313f60
Bryan Pardo <devpardo@gmail.com>
2023-06-24 12:03:02 +0800

    Update Navbar.jsx
    Toggle class to body prevent scrolling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,10 @@ import { Link } from 'react-scroll';
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
-  const handleClick = () => setNav(!nav);
+  const handleClick = () => {
+    setNav(!nav);
+    document.body.classList.toggle("overflow-hidden");
+  };
 
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
