b1a58e377ded2181508d0a09811eb74f25a0ce61
TanpreetJolly <tanpreet2002@gmail.com>
2023-10-17 17:17:59 +0530

    feat: text containing // wrapped in {} following eslint regulations

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -17,7 +17,7 @@ const Skills = () => {
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
           <div>
               <p className='text-4xl font-bold inline border-b-4 border-pink-600 '>Skills</p>
-              <p className='py-4'>// These are the technologies I've worked with</p>
+              <p className='py-4'>{"// These are the technologies I've worked with"}</p>
           </div>
 
           <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -15,13 +15,13 @@ const Work = () => {
           <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
             Work
           </p>
-          <p className='py-6'>// Check out some of my recent work</p>
+          <p className='py-6'>{"// Check out some of my recent work"}</p>
         </div>
 
 {/* container for projects */}
 <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           
-          {/* Grid Item */}
+          {/* Grd Item */}
           {project.map((item, index) => (
   <div
     key={index}
